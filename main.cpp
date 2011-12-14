5476060f74473e6f0e5837e35ee6b2891831f0da
Logan Capaldo <logancapaldo@gmail.com>
2011-12-14 21:04:38 +0000

    I don't even remember

--- main.cpp
+++ main.cpp
@@ -14,10 +14,22 @@ bool get_prop(const std::string& propkey, Out<std::string> val)
 }
 
   
+bool get_prop2(const std::string& propkey, std::string& val)
+{
+  if( propkey == "name" )
+  {
+     val = "logan";
+     return true;
+  }
+
+  return false;
+}
+
 
 int main()
 {
   std::string pval;
   get_prop("name", out( pval ));   
+  get_prop2("name",  pval );   
   std::cout << pval << "\n"; 
 }
